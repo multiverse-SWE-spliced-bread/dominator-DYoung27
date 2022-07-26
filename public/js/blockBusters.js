c8c499b691eceaf1f50a1b2a8d50aac3a8d7453c
Dyoung27 <DilipYoung27@gmail.com>
2022-07-26 10:52:01 +0100

    block-buster commit 4

--- public/js/blockBusters.js
+++ public/js/blockBusters.js
@@ -1,6 +1,8 @@
 const contain = document.getElementById('blocks')
+const block = document.getElementById('block-1')
+block.addEventListener("mouseover", function(){block.style.visibility = 'hidden'})
 
-for (i = 2; i < 100; i++) {
+for (i = 1; i <= 100; i++) {
 
     // Create each box element with class and id
     const el = document.createElement('div')
