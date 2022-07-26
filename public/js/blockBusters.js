aacd737b0eebd8cd752aa8c5ccdd1a6e752f232f
Dyoung27 <DilipYoung27@gmail.com>
2022-07-26 02:56:52 +0100

    block-buster commit

---
+++ public/js/blockBusters.js
@@ -0,0 +1,15 @@
+const contain = document.getElementById('blocks')
+
+for (i = 2; i <= 100; i++) {
+
+    // Create each box element with class and id
+    const el = document.createElement('div')
+
+    el.className = 'red-block'
+    el.id = `block-${i}`
+
+    contain.appendChild(el)
+
+    // Make the cursor invisible for boxes
+    el.style.cursor = 'none';
+}
