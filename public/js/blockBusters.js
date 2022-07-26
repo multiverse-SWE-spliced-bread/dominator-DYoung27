93c6764fb5ffd0bbe8b41390ae0f3bf66a1b09ed
Dyoung27 <DilipYoung27@gmail.com>
2022-07-26 11:27:37 +0100

    block-buster commit 7

--- public/js/blockBusters.js
+++ public/js/blockBusters.js
@@ -2,16 +2,17 @@ const contain = document.getElementById('blocks')
 const block = document.getElementById('block-1')
 block.addEventListener("mouseover", function(){block.remove()})
 
-for (i = 1; i <= 100; i++) {
+for (i = 2; i <= 100; i++) {
 
     // Create each box element with class and id
     const el = document.createElement('div')
 
-    el.className = 'red-block'
-    el.id = `block-${i}`
-
-    contain.appendChild(el)
+    el.setAttribute('class','red-block')
+    el.setAttribute ('id',`block-${i}`)
 
     // Make the cursor invisible for boxes
     el.addEventListener("mouseover", function(){el.remove()});
+
+    contain.appendChild(el)
+
 }
